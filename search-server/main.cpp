e1ed48ab9fb188292a0b0ea43d9f97dc9cc44d04
Vyacheslav Fedorov <slava@fuodorov.ru>
2023-03-05 07:06:32 +0000

    Add Process Queries

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,30 +1,5 @@
-#include "request_queue.h"
-#include "search_server.h"
-#include "remove_duplicates.h"
+using namespace std;
 
 int main() {
-    SearchServer search_server("and with"s);
-
-    search_server.AddDocument(1, "funny pet and nasty rat"s,
-                              DocumentStatus::ACTUAL, {7, 2, 7});
-    search_server.AddDocument(2, "funny pet with curly hair"s,
-                              DocumentStatus::ACTUAL, {1, 2});
-    search_server.AddDocument(3, "funny pet with curly hair"s,
-                              DocumentStatus::ACTUAL, {1, 2});
-    search_server.AddDocument(4, "funny pet and curly hair"s,
-                              DocumentStatus::ACTUAL, {1, 2});
-    search_server.AddDocument(5, "funny funny pet and nasty nasty rat"s,
-                              DocumentStatus::ACTUAL, {1, 2});
-    search_server.AddDocument(6, "funny pet and not very nasty rat"s,
-                              DocumentStatus::ACTUAL, {1, 2});
-    search_server.AddDocument(7, "very nasty rat and not very funny pet"s,
-                              DocumentStatus::ACTUAL, {1, 2});
-    search_server.AddDocument(8, "pet with rat and rat and rat"s,
-                              DocumentStatus::ACTUAL, {1, 2});
-    search_server.AddDocument(9, "nasty rat with curly hair"s,
-                              DocumentStatus::ACTUAL, {1, 2});
-
-    std::cout << "Before duplicates removed: "s << search_server.GetDocumentCount() << std::endl;
-    RemoveDuplicates(search_server);
-    std::cout << "After duplicates removed: "s << search_server.GetDocumentCount() << std::endl;
+    return 0;
 }
