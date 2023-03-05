1e98e8aca7978edfca3a8e3832ec07b972c5a90f
Vyacheslav Fedorov <slava@fuodorov.ru>
2023-03-05 07:14:16 +0000

    ProcessQueriesJoined

--- search-server/process_queries.h
+++ search-server/process_queries.h
@@ -7,3 +7,4 @@
 #include <execution>
 
 std::vector<std::vector<Document>> ProcessQueries(const SearchServer& search_server, const std::vector<std::string>& queries);
+std::vector<Document> ProcessQueriesJoined(const SearchServer& search_server, const std::vector<std::string>& queries);
